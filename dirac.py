53a9a043b841f644e91d98410cf116cb85471d6b
thierrydutoit <31282663+thierrydutoit@users.noreply.github.com>
2022-05-26 18:49:52 -0400

    Update dirac.py

--- dirac.py
+++ dirac.py
@@ -4,13 +4,11 @@ from matplotlib.pyplot import *
 import matplotlib.patches as mpatches
 
 st.title('Measuring time content')
-
 st.markdown('''Suppose you are given a black box with some unknown signal $f(t)$ in it, and the only 
                thing you can do is to provide another signal $in(t)$ as input, in which case the black 
-               box will ouput the scalar product $<in(t),f(t)>$.
-               
-               What kind of input signal should you use to get the value of $f(\Delta)$? 
-               ''')
+               box will ouput the scalar product $<in(t),f(t)>$. ''')
+st.markdown('''What kind of input signal should you use to get the value of $f(\Delta)$? ''')
+
 col1, col2 = st.columns(2)
 with col1:
    a=st.slider('Amplification: a', 1.0, 20.0, 1.0)
