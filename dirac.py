4f3367f5f48e7e722d2001f3eaf4b75fcfc97b3e
thierrydutoit <31282663+thierrydutoit@users.noreply.github.com>
2022-08-26 13:11:37 +0200

    Update dirac.py

--- dirac.py
+++ dirac.py
@@ -60,7 +60,7 @@ with col1:
    st.pyplot(fig)
 
 with col2:
-      fig,ax = subplots(figsize=(3,3))
+   fig,ax = subplots(figsize=(3,3))
    xlim(-3,3); ylim(-2, 2)
    plot(t,cos(3*t),'--')
    title(r'$f(t)=a\ cos(3t)$')
